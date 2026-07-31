6e51323ca9a745111a76dd432223113ea77b403d
jeromebaby <jeromebabysvbh@gmail.com>
2026-07-31 15:52:08 +0530

    Update test.py

--- src/anomaly_module/test.py
+++ src/anomaly_module/test.py
@@ -1,21 +1,33 @@
-from anomaly_module import AnomalyDetector, load_anomaly_inputs, update_sliding_window
-
-detector = AnomalyDetector()
+from anomaly_module import AnomalyDetector, load_anomaly_inputs, update_sliding_window, get_user_entry_count
 
 
-inputs = load_anomaly_inputs(user_id=1)
+total_days = get_user_entry_count(user_id)
 
-for data in inputs:
+if total_days>=21:
+    detector = AnomalyDetector()
+    inputs = load_anomaly_inputs(user_id)
+    
+    for data in inputs:
+    
+        risk_output, baseline_action, finalized_window = detector.evaluate_parameter(
+            param_name=data['param_name'],
+            current_value=data['current_value'],
+            baseline=data['baseline'],
+            std_dev=data['std_dev'],
+            historical_window=data['historical_window'],
+            max_window_length=data['max_window_length']
+        )        
+    
+        update_sliding_window(user_id=1, param_name=data['param_name'], updated_window=finalized_window)
 
-    risk_output, baseline_action, finalized_window = detector.evaluate_parameter(
-        param_name=data['param_name'],
-        current_value=data['current_value'],
-        baseline=data['baseline'],
-        std_dev=data['std_dev'],
-        historical_window=data['historical_window'],
-        max_window_length=data['max_window_length']
-    )
+else:
+        days_remaining = 27 - total_days
+        print(f"User {user_id} only has {total_days} entries. Skipping Anomaly Detection. "
+              f"Needs {days_remaining} more days to establish a stable baseline.")
     
 
-    update_sliding_window(user_id=1, param_name=data['param_name'], updated_window=finalized_window)
+
+
+
+
     
